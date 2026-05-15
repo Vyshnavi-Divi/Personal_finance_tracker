c53ac92cf1e21874a016c2909214d5e413c91e89
vyshnavi divi <vyshnavidivi1216@gmail.com>
2026-05-15 08:55:25 +0530

    Update Streamlit app features 2

--- home.py
+++ home.py
@@ -3,7 +3,7 @@ from datetime import *
 
 class FinanceTracker:
     def __init__(self):
-        self.transactions = []
+        self.transaction = []
 
     def add_income(self, source, amount, date, description=""):
         if amount <= 0:
